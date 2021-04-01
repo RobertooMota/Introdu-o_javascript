1cd4774e43d482898623862e431f92f2f593a411
RobertooMota <robertomota.contato@gmail.com>
2021-04-01 00:53:41 -0300

    Ferramentas do JS

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/*var nome = "Roberto Mota";
+var idade = 31;
+var idade_futura = 32;
+alert   ("Ola " + nome + " voce está com " + idade + " anos e fará " + idade_futura);
+alert   (nome + idade + idade_futura); //Concatena ao inves de somar.
+alert   (idade + idade_futura); // soma somente
+console.log(idade);*/
+
+
+
+/*
+var carros = [  {Marca:"Volks", nome:"fusca", ano: "1978"},
+                {Marca:"Volks", nome:"voyage", ano: "1988"},
+                {Marca:"Volks", nome:"gol", ano: "2000"},
+                {Marca:"Volks", nome:"sei la", ano: "2020"}]
+console.log(carros[1].nome);*/
