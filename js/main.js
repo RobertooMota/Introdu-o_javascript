696d0897ed75c2a8ac5f80d5c4caa18b407a6796
RobertooMota <robertomota.contato@gmail.com>
2021-04-02 01:35:53 -0300

    interação do JS com HTML

--- js/main.js
+++ js/main.js
@@ -1,3 +1,112 @@
+
+
+
+
+function clicou() {
+    document.getElementById("agradecimento").innerHTML = "<b>Vai pá ondi?</b>";
+}
+
+
+
+
+
+
+
+
+/*
+function clicou() {
+    var v = prompt("Insira o vcc:");
+    var i = prompt("Insira a corrente:")
+    var resultado = v * i;
+    alert("O valor da potência é de: " + resultado + " watts");
+}
+
+
+
+/*
+
+function verificaIdade(idade) {
+    var verificar;
+    if (idade >= 18) {
+        verificar = true;
+        return "Maior de Idade";
+    }else{
+        verificar = false;
+        return "Menor de Idade";
+    }
+}
+
+var idade_input = prompt("Digite sua Idade:");
+alert(verificaIdade(idade_input));
+
+
+
+
+/*
+function frase_output(frase, nome1, nome2){
+    return frase.replace(nome1,nome2);
+}
+
+
+alert(frase_output("Não estou bem", "Não", "Com certeza"));
+
+
+
+
+/*
+var num = prompt("Insira o valor do raio!");
+
+function area(raio) {
+    return 2*3.14*raio
+}
+alert(area(num));
+
+
+
+
+
+
+/*var data = new Date();
+alert(data);
+alert(data.getHours());
+//  alert(data.getMonth()+1)
+
+
+
+/*
+var x;
+for(x = 0; x < 7; x++){
+    console.log(x);
+}
+*/
+
+
+
+
+
+
+/*
+while (num < 10) {
+    console.log(num);
+    num++;
+    var num = 0;
+}
+
+*/
+
+
+
+/*var idade = prompt("Qual é a sua idade?");
+if (idade >= 18) {
+    alert("Maior de Idade!")
+}else{
+    alert("Menor de Idade!")
+}
+*/
+
+
+
+
 /*var nome = "Roberto Mota";
 var idade = 31;
 var idade_futura = 32;
@@ -10,7 +119,7 @@ console.log(idade);*/
 
 /*
 var carros = [  {Marca:"Volks", nome:"fusca", ano: "1978"},
-                {Marca:"Volks", nome:"voyage", ano: "1988"},
-                {Marca:"Volks", nome:"gol", ano: "2000"},
-                {Marca:"Volks", nome:"sei la", ano: "2020"}]
+{Marca:"Volks", nome:"voyage", ano: "1988"},
+{Marca:"Volks", nome:"gol", ano: "2000"},
+{Marca:"Volks", nome:"sei la", ano: "2020"}]
 console.log(carros[1].nome);*/
